e4c873133920052f0d78d3e7fa7e6244402a118e
DrNickker <mintagirov2015@yandex.ru>
2022-07-22 16:04:28 +0500

    кодировка при запросе

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,7 +3,28 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest;
 
+    const formData = new FormData();
+    let queryParams = '';
+    if (options.data !==undefined) {
+        if (options.method === 'GET') {
+            queryParams = '?' + Object.entries(options.data).map(
+                ([Key, value]) => '${encodeURIComponent(key)}=${encodeURIComponent(value)}'
+             ).join('&');
+        } else {
+            Object.entries(options.data).forEach(v => formData.append(...v));
+        }
+    }
+
+
+
+
+
+    
+
+xhr.open(options.method, options.url + queryParams);
+xhr.send(formData);
     
 
 };
